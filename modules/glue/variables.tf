51aef9b0a7d38450f6f69b9c360814c0579a9175
Ahmed Khan <khan.ahmed.m@outlook.com>
2025-03-08 10:29:16 -0600

    glue terraform input variables

---
+++ modules/glue/variables.tf
@@ -0,0 +1,10 @@
+variable "glue_script_bucket_name" {
+  description = "The S3 bucket where the Glue script will be stored"
+  type        = string
+}
+
+variable "financial_raw_data_bucket_name" {
+  description = "The S3 bucket where the raw data will be stored"
+  type        = string
+}
+
