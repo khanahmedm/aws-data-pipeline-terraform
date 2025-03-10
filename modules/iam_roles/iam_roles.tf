90b78ba03ededff856b349edde587030ec804925
Ahmed Khan <khan.ahmed.m@outlook.com>
2025-03-10 13:54:19 -0500

    update iam roles

--- modules/iam_roles/iam_roles.tf
+++ modules/iam_roles/iam_roles.tf
@@ -58,3 +58,77 @@ resource "aws_iam_role_policy_attachment" "lambda_ssm_attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
 }
+
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name = "terraform_secrets_manager_policy"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ],
+        Resource = "arn:aws:secretsmanager:us-east-1:585768163077:secret:financialdb_password-*"
+      }
+    ]
+  })
+}
+
+/*
+resource "aws_iam_role" "step_functions_role" {
+  name = "step_functions_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "step_functions_policy" {
+  name = "step_functions_execution_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Resource = [
+          var.extract_data_lambda_arn,
+          var.trigger_glue_crawler_lambda_arn
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:StartJobRun"
+        ]
+        Resource = [
+          var.glue_etl_job_arn
+        ]
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "step_functions_policy_attach" {
+  role       = aws_iam_role.step_functions_role.name
+  policy_arn = aws_iam_policy.step_functions_policy.arn
+}
+*/
